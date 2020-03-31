3506266f5b2698422916b5db26dc9d4a3dd076ee
Don Wibier <don@anykey.nl>
2020-03-31 17:45:50 +0200

    Everything upgraded to DX v20.1.2 EAP and .NET preview 5

--- DX.Test.Web.Core/Startup.cs
+++ DX.Test.Web.Core/Startup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
+//using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
